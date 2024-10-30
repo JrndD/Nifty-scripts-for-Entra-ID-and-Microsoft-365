39a1f0262370053c6391e9d6aceb5018930edf54
Jørund Dahl <83746585+JrndD@users.noreply.github.com>
2024-10-30 21:42:16 +0200

    Create applicationConsentWith180dSignIns.ps1

---
+++ applicationConsentWith180dSignIns.ps1
@@ -0,0 +1,79 @@
+#region Get Application Sign In from Log Analytics
+
+$tenantId = <tenantId>
+$subscription = <subscription>
+$resourceGroupName = <resourceGroupName>
+$logAnalyticsWorkspace = <logAnalyticsWorkspace>
+
+Connect-AzAccount -Tenant $tenantId
+set-AzContext $subscription
+$workspace = Get-AzOperationalInsightsWorkspace -ResourceGroupName $resourceGroupName -Name $logAnalyticsWorkspace
+
+$kqlQuery = '
+AADServicePrincipalSignInLogs
+| where TimeGenerated > ago(180d)
+| where ResultType == 0
+| summarize count() by AppId
+'
+$AADServicePrincipalSignInLogs = Invoke-AzOperationalInsightsQuery -Workspace $Workspace -Query $kqlQuery
+
+$kqlQuery = '
+AADNonInteractiveUserSignInLogs
+| where TimeGenerated > ago(180d)
+| where ResultType == 0
+| summarize count() by AppId
+'
+$AADNonInteractiveUserSignInLogs = Invoke-AzOperationalInsightsQuery -Workspace $Workspace -Query $kqlQuery
+
+$kqlQuery = '
+SigninLogs
+| where TimeGenerated > ago(180d)
+| where ResultType == 0
+| summarize count() by AppId
+'
+$SigninLogs = Invoke-AzOperationalInsightsQuery -Workspace $Workspace -Query $kqlQuery
+
+$signinlogshash = @{}
+$signinlogs.results | ForEach-Object {
+    if($signinlogshash[$_."AppId"]) {
+        $signinlogshash[$_."AppId"] += [int]$_."count_"
+    }
+    else {
+        $signinlogshash.Add($_."AppId",[int]$_."count_")
+    }
+}
+
+$AADServicePrincipalSignInLogs.results | ForEach-Object {
+    if($signinlogshash[$_."AppId"]) {
+        $signinlogshash[$_."AppId"] += [int]$_."count_"
+    }
+    else {
+        $signinlogshash.Add($_."AppId",[int]$_."count_")
+    }
+}
+
+$AADNonInteractiveUserSignInLogs.results | ForEach-Object {
+    if($signinlogshash[$_."AppId"]) {
+        $signinlogshash[$_."AppId"] += [int]$_."count_"
+    }
+    else {
+        $signinlogshash.Add($_."AppId",[int]$_."count_")
+    }
+}
+
+#endregion Get Application Sign In from Log Analytics
+
+#region Generate application consent report and add sign in count to it
+$appConsent = Export-MsIdAppConsentGrantReport -ReportOutputType PowerShellObjects
+
+$appConsent | ForEach-Object {
+    if($signinlogshash[$_.AppId]) {
+        $_ | Add-Member -MemberType NoteProperty -Name SignInCount -Value $signinlogshash[$_.AppId]
+    }
+    else {
+        $_ | Add-Member -MemberType NoteProperty -Name SignInCount -Value 0
+    }
+}
+
+$appConsent | Export-Excel '.\appConsentReportWithSignIns.xlsx'
+#endregion Generate application consent report and add sign in count to it
